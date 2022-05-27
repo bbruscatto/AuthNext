9632217e4666feeebee5ca0a54d58f0986809d2c
bbruscatto <bruscatto@gmail.com>
2022-05-26 23:25:11 -0300

    feat/Auth

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
 import Router from "next/router";
-import { parseCookies, setCookie } from "nookies";
+import { destroyCookie, parseCookies, setCookie } from "nookies";
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { api } from "../services/api";
+import { api } from "../services/apiClient";
 
 type User = {
     email: string;
@@ -26,6 +26,13 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+    destroyCookie(undefined, 'nextauth.token')
+    destroyCookie(undefined, 'nextauth.refreshToken')
+
+    Router.push('/')
+}
+
 export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<User>()
     const isAuthenticated = !!user;
@@ -40,6 +47,9 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
                 setUser({ email, permissions, roles })
             })
+                .catch(() => {
+                    signOut()
+                })
         }
     }, [])
     async function signIn({ email, password }: SignInCredentials) {
